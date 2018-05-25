012c54d936e4946fbd05c13b0660a9528107ce17
gabrielle-lau <laumingyi@gmail.com>
2018-05-24 22:44:10 -0500

    done VR button for music

--- src/TextScene.js
+++ src/TextScene.js
@@ -8,28 +8,28 @@ export default class TextScene extends React.Component {
     render () {
         return (
             <View>
-                
-                <Text
-                    style={{
-                        backgroundColor: '#777879',
-                        fontSize: 0.5,
-                        fontWeight: '400',
-                        paddingLeft: 0.2,
-                        paddingRight: 0.2,
-                        textAlign: 'center',
-                        textAlignVertical: 'center',
-                        transform: [
-                            { translate: [-4, 1, -0.5] },
-                            { scale: 0.5 },
-                            { rotateY: 90 } 
-                        ]
-                    }}
-                >Piano music here
-                    <Sound
-                    source={{ wav: asset('MONO-036.wav') }}
-                    loop={true}
-                    />
-                </Text>
+                <VrButton
+                onClickSound={{ wav: asset('MONO-036.wav') }}
+                >
+                    <Text
+                        style={{
+                            backgroundColor: '#A482DF',
+                            borderRadius: 0.3,
+                            fontSize: 0.5,
+                            fontWeight: '400',
+                            paddingLeft: 0.2,
+                            paddingRight: 0.2,
+                            textAlign: 'center',
+                            textAlignVertical: 'center',
+                            transform: [
+                                { translate: [-4, 1, -0.5] },
+                                { scale: 0.5 },
+                                { rotateY: 90 } 
+                            ]
+                        }}
+                    >Click here for music
+                    </Text>
+                </VrButton>                
                 <Text
                     style={{
                         backgroundColor: '#A482DF',
@@ -42,7 +42,7 @@ export default class TextScene extends React.Component {
                         textAlignVertical: 'center',
                         transform: [
                             { translate: [1, 2.5, -12] },
-                            { scale: 1 }
+                            { scale: 0.5 }
                         ]
                     }}
                 >Loral-1300Com</Text>
